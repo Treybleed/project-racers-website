11b07e17218dec02c646eda4fb0c491de5f374d7
Matt <matthobbshome@gmail.com>
2021-02-14 21:02:03 +0000

    Updating styles name to match component

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from "react"
-import * as styles from  '../styles/home.module.scss';
+import * as styles from  '../styles/title-component.module.scss';
 
 type TitleProps = {
     title: string
